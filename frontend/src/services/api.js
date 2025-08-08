aad4fb1c6a2dcc1d659958a0e3502c57821ea086
Diksha Shamrao Kuthe <kuthediksha123@gmail.com>
2025-08-08 23:00:27 +0530

    update all changing files

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://whatsapp-clone-backend-coral.vercel.app/api';
+const API_URL = 'https://whatsapp-backend-six.vercel.app/api';
 export default API_URL;
 export const getConversations = () => axios.get(`${API_URL}/conversations`);
 export const getMessages = (wa_id) => axios.get(`${API_URL}/messages/${wa_id}`);
